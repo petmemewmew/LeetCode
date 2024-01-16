a1cb330ad5287b5ff088611997c030c0c6517402
Chenning Huang <90765384+petmemewmew@users.noreply.github.com>
2024-01-15 22:20:00 -0800

    Time: 113 ms (30.15%) | Memory: 7.1 MB (58.25%) - LeetSync

--- 1-two-sum/two-sum.c
+++ 1-two-sum/two-sum.c
@@ -3,17 +3,17 @@
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
 	int*array=(int*)malloc(sizeof(int)*2);
-    for(int i=0;i<numsSize;i++){//直接多个for循环暴力破解
-        for(int j =i+1;j<numsSize;j++){
-            if(nums[i]+nums[j]==target){
-                array[0]=i;
-                array[1]=j;
-                *returnSize=2;
+    for(int i = 0;i < numsSize; i++){
+        for(int j = i + 1; j < numsSize; j++){
+            if(nums[i] + nums[j] == target){
+                array[0] = i;
+                array[1] = j;
+                *returnSize = 2;
                 return array;
             }
         }
     }
-     	*returnSize=0;
-    	*array=NULL;
+     	*returnSize = 0;
+    	*array = NULL;
     	return array;
 }  
